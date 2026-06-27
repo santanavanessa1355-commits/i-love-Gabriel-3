fa9e50c120439ba9cdeb22dac7365acebe75d334
santanavanessa1355-commits <santanavanessa1355@gmail.com>
2026-06-27 01:17:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function entrar() {
+  window.location.href = "espaco.html";
+}
+
+let contadorEstrela = 0;
+
+function clicarEstrela() {
+  contadorEstrela++;
+
+  if (contadorEstrela === 5) {
+    window.location.href = "elogio.html";
+  }
+}
+
+function continuar() {
+  window.location.href = "fotos.html";
+}
+
+function irParaAmor() {
+  window.location.href = "amor.html";
+}
